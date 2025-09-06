9370ae3128f7d5ccd70c90d2f7066864966e5d95
Ariyan Aftab Spandan <ariyanspandan@gmail.com>
2025-09-06 11:03:06 +0600

    Fix parser error by correcting Site.Master.cs class name to SiteMaster

--- Site.Master.cs
+++ Site.Master.cs
@@ -3,7 +3,7 @@ using System.Web.UI;
 
 namespace WebApplication1
 {
-    public partial class Site : MasterPage
+    public partial class SiteMaster : MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
